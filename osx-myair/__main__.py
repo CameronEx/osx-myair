50dc72d5dc708113d50d7503837aaa0f0ae74943
Cameron Exley <github@exley.com.au>
2018-01-28 12:21:11 +1100

    Logging, logging, logging.

--- osx-myair/__main__.py
+++ osx-myair/__main__.py
@@ -1,4 +1,5 @@
 import configparser
+import logging
 import os
 import rumps
 import requests
@@ -6,31 +7,51 @@ import requests
 class MyAirTaskbar(rumps.App):
 
     def __init__(self):
+        logger.debug("Initialising. Calling super.")
+        super().__init__()
+
         # Setup the config parser
+        logger.debug("Setting up the config parser.")
         config = configparser.ConfigParser()
         config_dir = os.path.expandpath("~/.config/osx-myair/")
         config_file = config_dir + "config.ini"
 
         # Ensure the config directory and file exists, if not, create defaults
+        logger.debug("Checking if the configuration directory exists.")
         if not os.path.exists(config_dir):
+            logger.info("Configuration directory Does not exist. Creating it.")
             os.makedirs(config_dir)
-        if not config.read(config_dir + config_file):
-            self.config['DEFAULT'] = { "pollInterval": "30",
+        else:
+            logger.debug("Configuration directory exists.")
+
+        logger.debug("Checking if the configuration directory exists.")
+        if not config.read(config_file):
+            logger.info("Configuration file did not exist, creating one with defaults.")
+            default_config = { "pollInterval": "30",
                                 "server": "",
                                 "port": 2025}
+            logger.debug("Default config is: {}".format(default_config))
+            config['DEFAULT'] = default_config
             with open(config_file, "w") as new_file:
+                logger.debug("Saving configuration file to {}.".format(config_file))
                 self.config.write(new_file)
 
         # Read the config file and set state
+        logger.info("Loading configuration.")
         self.poll_int = config['DEFAULT']['pollInterval']
+        logger.debug("Poll inteval: {}".format(self.poll_int))
         self.server = self.config['DEFAULT']['server']
+        logger.debug("Server IP: {}".format(self.server))
         self.port = self.config['DEFAULT']['port']
+        logger.debug("API port: {}".format(self.port))
 
         self.current_state = {}
         self.base_target = "http://{server}:{port}/".format(**self)
+        logger.debug("Built base URL: {}".format(self.base_target))
 
         # Get the current state of the air conditioner
-        get_state()
+        logger.info("Getting intial state.")
+        self.get_state()
 
     class ResponseError(Exception):
         pass
@@ -43,26 +64,33 @@ class MyAirTaskbar(rumps.App):
         """
 
         if not self.server:
+            logger.error("No server has been defined.")
             rumps.alert(title="Configuration Error", message="No host is configured.\nError 01")
+
         target = self.base_target + "getSystemData"
+        logger.debug("Built target URL {}"(.format(target)))
         self.current_state = send_command(target)
 
     def send_command(self, target):
         """
         Sends command to API. Handles responses.
         """
+
+        logger.debug("Attempting to send command: {}".format(target))
         try:
             response = requests.get(target)
             if response.status_code == 200:
+                logger.debug("Expected response recieved.\n Returning data:\n {}".format(response.json()))
                 return response.json()
             else:
+                logger.error("Unexpected response recived.")
                 raise ResponseError
         except ConnectionError as error:
-            # FIXME This will be super annoying, change it to logging
-            # rumps.alert(title="Connection Error!", message="Unable to connect to host.\nError 02")
+            logging.error("Unable to connect to host.")
             return None
         except ResponseError:
-            # FIXME Log the issue
+            logger.error("Status code was: {}".format(response.status_code))
+            logger.debug("Body of resposne was:\n{}".format(response.text))
             return None
 
     @rumps.clicked("On/Off")
@@ -70,14 +98,23 @@ class MyAirTaskbar(rumps.App):
         """
         Turns aircon on or off. Checks state first.
         """
-        if self.current_state["ac1"]["info"]["state"] == on:
+        logger.debug("Attempting to toggle AC state.")
+        if self.current_state["ac1"]["info"]["state"] == "on":
+            logger.debug("Last know state of AC was on.")
+            logger.info("Turning AC off.")
             target = self.base_target + 'setAircon?json={"ac1":{"info":{"state":"off"}}}'
         else:
+            logger.debug("Last know state of AC was off.")
+            logger.info("Turning AC on.")
             target = self.base_target + 'setAircon?json={"ac1":{"info":{"state":"on"}}}'
 
-        send_command(target)
-        # FIXME If successful, pop a notification
-        rumps.notification(title="MyAir", message="AC1 turned on successfully")
+        logger.debug("Built target URL {}"(.format(target)))
+        if send_command(target):
+            self.get_state()
+            if self.current_state["ac1"]["info"]["state"] == "on":
+                rumps.notification(title="MyAir", message="AC turned on successfully")
+            else:
+                rumps.notification(title="MyAir", message="AC turned off successfully")
 
     @rumps.clicked("About")
     def about(self, _):
@@ -86,6 +123,15 @@ class MyAirTaskbar(rumps.App):
                     "Maintained at https://github.com/CameronEx/osx-myair"
                     "Not affiliated with Advantage Air.")
 
+# Configure the logger
+# FIXME: Log to file, for when program is compiled to .app
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 if __name__ == "__main__":
     MyAirTaskbar("MyAir").run()
